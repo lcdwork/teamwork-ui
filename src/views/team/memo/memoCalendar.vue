0d12a47b0231cd0c68b1a08e8ac76ee1420a0be3
lcd <lcdwork@163.com>
2020-03-19 20:34:51 +0800

    备忘录日历完成，备忘录功能页面拆分

--- src/views/team/memo/memoCalendar.vue
+++ src/views/team/memo/memoCalendar.vue
@@ -2,32 +2,11 @@
   <div>
     <el-calendar v-model="calendarDate" id="calendar">
       <template slot="dateCell" slot-scope="{date, data}">
-<!--        <el-popover-->
-<!--          placement="top"-->
-<!--          title="书写备忘录"-->
-<!--          width="240"-->
-<!--          trigger="click">-->
-<!--          <el-input v-model="memo.content" type="textarea" :autosize="{ minRows: 5, maxRows: 10}" placeholder="备忘录" />-->
-<!--          <div style="text-align: right; margin: 5px 0 -5px;">-->
-<!--            <el-button type="primary" size="mini" @click="submitContent">保存</el-button>-->
-<!--          </div>-->
-<!--          <div slot="reference" ref="day" @click="editContent(data.day)">-->
-<!--            <span>{{ data.day.split('-').slice(2).join('-') }}</span><br>-->
-<!--            <div class="cal-day-content" :style="{'width': dayWeight + 'px', 'height':dayHeight + 'px'}">-->
-<!--              <div v-for="item in calendarData">-->
-<!--                <div v-if="(item.time).indexOf(data.day)!=-1">-->
-<!--                  <span>&emsp;&emsp;{{item.memoContent}}-->
-<!--                  </span>-->
-<!--                </div>-->
-<!--              </div>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--        </el-popover>-->
         <div ref="day" @click="editContent(data.day)">
           <span>{{ data.day.split('-').slice(2).join('-') }}</span><br>
           <div class="cal-day-content" :style="{'width': dayWeight + 'px', 'height':dayHeight + 'px'}">
             <div v-for="item in calendarData">
-              <div v-if="(item.time).indexOf(data.day)!=-1">
+              <div v-if="(item.memoTime).indexOf(data.day)!=-1">
                   <span>&emsp;&emsp;{{item.memoContent}}
                   </span>
               </div>
@@ -48,6 +27,7 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { getMemo, addMemo, updateMemo} from "@/api/memo";
 export default {
   name: "memoCalendar",
   computed: {
@@ -65,39 +45,34 @@ export default {
         endDate: null
       },
       memo: {
-        day: null,
+        memoTime: null,
         memoContent: null,
       },
       dayHeight: 40,
       dayWeight: 100,
       calendarDate: new Date(),
-      calendarData: [
-        {
-          time: '2020-03-19',
-          id: 111,
-          memoContent: '这是一段测试文字，测试文字长度。这是一段测试文字，测试文字长度。这是一段测试文字，测试文字长度。这是一段测试文字，测试文字长度。这是一段测试文字，测试文字长度。'
-        }
-      ]
+      calendarData: []
     }
   },
   created() {
     this.calendarSearch.userId = this.loginUserId
+    this.getMemoListWithFormat(new Date())
     this.$nextTick(() => {
       this.dayHeight = this.$refs.day.offsetHeight - 10
       this.dayWeight = this.$refs.day.offsetWidth
       // 点击前一个月
       let prevBtn = document.querySelector('.el-calendar__button-group .el-button-group>button:nth-child(1)')
       prevBtn.addEventListener('click', () => {
-        this.getListByTime(this.calendarDate)
+        this.getMemoListWithFormat(this.calendarDate)
       })
       let todayBtn = document.querySelector('.el-calendar__button-group .el-button-group>button:nth-child(2)')
       todayBtn.addEventListener('click', () => {
-        this.getListByTime(this.calendarDate)
+        this.getMemoListWithFormat(this.calendarDate)
       })
       // 点击下一个月
       let nextBtn = document.querySelector('.el-calendar__button-group .el-button-group>button:nth-child(3)')
       nextBtn.addEventListener('click', () => {
-        this.getListByTime(this.calendarDate)
+        this.getMemoListWithFormat(this.calendarDate)
       })
     })
   },
@@ -111,21 +86,19 @@ export default {
         .catch(_ => {})
     },
     editContent(day) {
-      if (day !== this.memo.time) {
-        var data = this.calendarData.find(v => v.time === day)
-        if(data !== undefined) {
-          this.memo = data
-        } else {
-          this.memo = {
-            id: null,
-            content: null,
-            time: day
-          }
+      var data = this.calendarData.find(v => v.memoTime === day)
+      if(data !== undefined) {
+        this.memo = data
+      } else {
+        this.memo = {
+          memoId: null,
+          memoContent: null,
+          memoTime: day
         }
       }
       this.dialogVisible = true
     },
-    getListByTime(time) {
+    getMemoListWithFormat(time) {
       var date = new Date(time);
       var year = date.getFullYear();
       var month= date.getMonth()+1<10 ? "0"+(date.getMonth()+1) : date.getMonth()+1;
@@ -133,12 +106,37 @@ export default {
       var lastDay = new Date(year,date.getMonth()+1,0).getDate()
       this.calendarSearch.startDate = year+"-"+month+"-01"
       this.calendarSearch.endDate =  year+"-"+month+"-"+lastDay
-      listByTime(this.calendarSearch).then(response => {
+      this.getMemoList()
+    },
+    getMemoList() {
+      getMemo(this.calendarSearch).then(response => {
+        console.log(response.rows)
         this.calendarData = response.rows
       })
     },
-    submitForm() {
-      this.dialogVisible = false
+    submitForm: function() {
+      console.log(this.memo)
+      if (this.memo.memoId != undefined) {
+        updateMemo(this.memo).then(response => {
+          if (response.code === 200) {
+            this.msgSuccess("修改成功");
+            this.dialogVisible = false;
+            this.getMemoList()
+          } else {
+            this.msgError(response.msg);
+          }
+        });
+      } else {
+        addMemo(this.memo).then(response => {
+          if (response.code === 200) {
+            this.msgSuccess("新增成功");
+            this.dialogVisible = false;
+            this.getMemoList()
+          } else {
+            this.msgError(response.msg);
+          }
+        });
+      }
     }
   }
 }
